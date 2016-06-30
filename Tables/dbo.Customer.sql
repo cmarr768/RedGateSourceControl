dd78ed509ebb5f0752a5744fe8d55131bc17ee42
unknown <Family@DESKTOP-GF3VRUM>
2016-06-30 14:18:13 -0400

    Adding state field to customer.

--- Tables/dbo.Customer.sql
+++ Tables/dbo.Customer.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[Customer]
 (
 [CustomerID] [int] NOT NULL IDENTITY(1, 1),
 [FirstName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[LastName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+[LastName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[StateCode] [varchar] (5) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Customer] ADD CONSTRAINT [PK__Customer__A4AE64B87F5EF2C6] PRIMARY KEY CLUSTERED  ([CustomerID]) ON [PRIMARY]
